0b93fcc6a4202f02e5ac64be74ff5382455c8b63
miemouridsen <miemouridsen@hotmail.com>
2021-11-11 11:38:36 +0100

    Buzzer with interface added, no methods implemented

---
+++ Microwave.Classes/Boundary/Buzzer.cs
@@ -0,0 +1,23 @@
+﻿using Microwave.Classes.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Microwave.Classes.Boundary
+{
+    public class Buzzer : IBuzzer
+    {
+        private IOutput myOutput;
+
+        public Buzzer(IOutput output)
+        {
+            myOutput = output;
+        }
+
+        public void MakeSound()
+        {
+            
+        }
+
+    }
+}
